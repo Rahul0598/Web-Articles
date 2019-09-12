677e5443a982e8b31c4b56c5cb372dc92846dfd1
scarydonut <praj4825@gmail.com>
2019-09-12 22:33:31 +0530

    created node-server

---
+++ node-server/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+    res.render('index.html')
+})
+
+app.listen(3000);
